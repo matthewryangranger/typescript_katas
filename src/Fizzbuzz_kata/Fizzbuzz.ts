92958e5dbcfbbcd3efbbe6636285780d64697edd
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-07 19:46:17 -0700

    @ f Basic multiples of three, five, seven, eleven cases pass

--- src/Fizzbuzz_kata/Fizzbuzz.ts
+++ src/Fizzbuzz_kata/Fizzbuzz.ts
@@ -1,13 +1,19 @@
 export class Fizzbuzz {
-    public convert(num:number): string {
-        if (num % 15 === 0){
+    public convert(num: number): string {
+        if (num % 15 === 0) {
             return "FizzBuzz";
         }
-        if (num % 3 === 0){
+        if (num % 3 === 0) {
             return "Fizz";
         }
-        if (num % 5 === 0){
+        if (num % 5 === 0) {
             return "Buzz";
+        }
+        if (num % 7 === 0) {
+            return "Whizz";
+        }
+        if (num % 11 === 0) {
+            return "Bang";
         }
         return num.toString();
     }
--- tests/Fizzbuzz_kata_tests/fizzbuzz.test.ts
+++ tests/Fizzbuzz_kata_tests/fizzbuzz.test.ts
@@ -20,12 +20,28 @@ describe("Fizzbuzz", () => {
         expect(unit.convert(5)).toBe("Buzz");
     });
 
+    it("should convert a number seven to string Whizz", () => {
+        expect(unit.convert(7)).toBe("Whizz");
+    });
+
+    it("should convert a number eleven to string Bang", () => {
+        expect(unit.convert(11)).toBe("Bang");
+    });
+
     it("should convert a multiple of three to string Fizz", () => {
         expect(unit.convert(6)).toBe("Fizz");
     });
 
     it("should convert a multiple of five to string Buzz", () => {
         expect(unit.convert(10)).toBe("Buzz");
+    });
+
+    it("should convert a multiple of seven to string Whizz", () => {
+        expect(unit.convert(49)).toBe("Whizz");
+    });
+
+    it("should convert a multiple of eleven to string Bang", () => {
+        expect(unit.convert(22)).toBe("Bang");
     });
 
     it("should convert a multiple of three and five to string FizzBuzz", () => {
