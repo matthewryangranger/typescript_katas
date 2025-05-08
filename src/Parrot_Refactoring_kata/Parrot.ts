a09bc560fd3d600706d216e016c4dcc1317ebe78
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-08 14:15:42 -0700

    @ r Safe delete unused Parrot and Parrot enum types:

--- src/Parrot_Refactoring_kata/Parrot.ts
+++ src/Parrot_Refactoring_kata/Parrot.ts
@@ -1,9 +1,3 @@
-export enum ParrotTypes {
-    EUROPEAN,
-    AFRICAN,
-    NORWEGIAN_BLUE,
-}
-
 export interface ParrotInterface {
     getSpeed(): number;
 
@@ -18,13 +12,15 @@ export class EuropeanParrot implements ParrotInterface {
     getCry(): string {
         return "Sqoork!";
     };
-}
+};
 
 export class AfricanParrot implements ParrotInterface {
     private coconutCount: number;
+
     constructor(private CNutCount: number = 0) {
         this.coconutCount = CNutCount;
     }
+
     public getSpeed(): number {
         return Math.max(0, 12 - 9 * this.coconutCount);
     };
@@ -32,57 +28,22 @@ export class AfricanParrot implements ParrotInterface {
     getCry(): string {
         return "Sqaark!";
     };
-}
+};
 
 export class NorwegianBlueParrot implements ParrotInterface {
     private isNailed: boolean;
     private voltage: number;
-    constructor(private isNailedBool: boolean = false, private volts: number = 0){
+
+    constructor(private isNailedBool: boolean = false, private volts: number = 0) {
         this.isNailed = isNailedBool;
         this.voltage = volts;
     }
+
     public getSpeed(): number {
         return (this.isNailed) ? 0 : Math.min(24, this.voltage * 12);
     };
 
     getCry(): string {
-        return  this.voltage > 0 ? "Bzzzzzz" : "...";
+        return this.voltage > 0 ? "Bzzzzzz" : "...";
     };
-}
-
-export class Parrot {
-    constructor(private parrotType: ParrotTypes,
-                private numberOfCoconuts: number,
-                private voltage: number,
-                private isNailed: boolean) {
-    }
-
-    public getSpeed(): number {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                let parrot = new EuropeanParrot();
-                return parrot.getSpeed();
-            case ParrotTypes.AFRICAN:
-                let parrot2 = new AfricanParrot(this.numberOfCoconuts);
-                return parrot2.getSpeed();
-            case ParrotTypes.NORWEGIAN_BLUE:
-                let parrot3 = new NorwegianBlueParrot(this.isNailed, this.voltage);
-                return parrot3.getSpeed();
-        }
-    }
-
-    public getCry(): String {
-        switch (this.parrotType) {
-            case ParrotTypes.EUROPEAN:
-                let parrot2 = new EuropeanParrot();
-                return parrot2.getCry();
-            case ParrotTypes.AFRICAN:
-                let parrot = new AfricanParrot();
-                return parrot.getCry();
-            case ParrotTypes.NORWEGIAN_BLUE:
-                let parrot3 = new NorwegianBlueParrot(this.isNailed, this.voltage);
-                return parrot3.getCry();
-        }
-    }
-
-}
+};
--- tests/Parrot_Refactoring_kata_tests/Parrot.spec.ts
+++ tests/Parrot_Refactoring_kata_tests/Parrot.spec.ts
@@ -1,59 +1,59 @@
 /* Specification file: Refactored code must pass these provided tests */
-import {Parrot, ParrotTypes} from "../../src/Parrot_Refactoring_kata/Parrot";
+import {EuropeanParrot, AfricanParrot, NorwegianBlueParrot} from "../../src/Parrot_Refactoring_kata/Parrot";
 
-describe('Parrot', () => {
+describe('ParrotInterface', () => {
 
     it('gets speed of European Parrot', () => {
-        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        const parrot = new EuropeanParrot();
         expect(parrot.getSpeed()).toBe(12);
     });
 
     it('gets speed of African Parrot with one coconut', () => {
-        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        const parrot = new AfricanParrot(1);
         expect(parrot.getSpeed()).toBe(3);
     });
 
     it('gets speed of African Parrot with two coconuts', () => {
-        const parrot = new Parrot(ParrotTypes.AFRICAN, 2, 0, false);
+        const parrot = new AfricanParrot(2);
         expect(parrot.getSpeed()).toBe(0);
     });
 
     it('gets speed of African Parrot with no coconuts', () => {
-        const parrot = new Parrot(ParrotTypes.AFRICAN, 0, 0, false);
+        const parrot = new AfricanParrot();
         expect(parrot.getSpeed()).toBe(12);
     });
 
     it('gets speed of Norwegian Blue Parrot nailed', () => {
-        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, true);
+        const parrot = new NorwegianBlueParrot(true);
         expect(parrot.getSpeed()).toBe(0);
     });
 
-    it('gets speed of Norwegian Blue Parrot not nailed', () => {
-        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 1.5, false);
+    it('gets speed of Norwegian Blue Parrot not nailed and 1.5 voltage', () => {
+        const parrot = new NorwegianBlueParrot(false, 1.5);
         expect(parrot.getSpeed()).toBe(18);
     });
 
-    it('gets speed of Norwegian Blue Parrot not nailed high voltage', () => {
-        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+    it('gets speed of Norwegian Blue Parrot not nailed with 4 volts', () => {
+        const parrot = new NorwegianBlueParrot(false, 4);
         expect(parrot.getSpeed()).toBe(24);
     });
 
     it('gets cry of European Parrot', () => {
-        const parrot = new Parrot(ParrotTypes.EUROPEAN, 0, 0, false);
+        const parrot = new EuropeanParrot();
         expect( parrot.getCry()).toBe("Sqoork!");
     });
 
     it('gets cry of African Parrot', () => {
-        const parrot = new Parrot(ParrotTypes.AFRICAN, 1, 0, false);
+        const parrot = new AfricanParrot();
         expect( parrot.getCry()).toBe("Sqaark!");
     });
     it('gets cry of Norwegian Blue with high voltage', () => {
-        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 4, false);
+        const parrot = new NorwegianBlueParrot( false, 4);
         expect( parrot.getCry()).toBe("Bzzzzzz");
     });
 
-    it('gets cry of NorwegianBlue without voltage', () => {
-        const parrot = new Parrot(ParrotTypes.NORWEGIAN_BLUE, 0, 0, false);
+    it('gets cry of NorwegianBlue without voltage and not nailed', () => {
+        const parrot = new NorwegianBlueParrot();
         expect(parrot.getCry()).toBe("...");
     });
 
