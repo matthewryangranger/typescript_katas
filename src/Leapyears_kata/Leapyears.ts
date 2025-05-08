e1f1ff21b6cdf6327c063c8cbcab6fe04b76839f
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-07 23:52:18 -0700

    @ f Added four cases for typical and atypical leap and common years, they pass and refactored

--- src/Leapyears_kata/Leapyears.ts
+++ src/Leapyears_kata/Leapyears.ts
@@ -1,6 +1,7 @@
 export class Leapyears {
     public revealIfLeapYear(num: number): boolean {
-        if (num === 4) return true;
-        return false;
+        if (num % 400 === 0) return true
+        if (num % 100 === 0) return false;
+        return (num % 4 === 0);
     }
 }
--- tests/Leapyears_kata_tests/leapyears.test.ts
+++ tests/Leapyears_kata_tests/leapyears.test.ts
@@ -5,11 +5,23 @@ describe("Leapyears", () => {
     const unit = new Leapyears();
 
     it("should return false for typical common year", () => {
-        expect(unit.revealIfLeapYear(1)).toBe(false);
+        expect(unit.revealIfLeapYear(2001)).toBe(false);
+        expect(unit.revealIfLeapYear(2025)).toBe(false);
     });
 
     it("should return true for typical leap year", () => {
-        expect(unit.revealIfLeapYear(4)).toBe(true);
+        expect(unit.revealIfLeapYear(2004)).toBe(true);
+        expect(unit.revealIfLeapYear(1996)).toBe(true);
+    });
+
+    it("should return false for an atypical common year", () => {
+        expect(unit.revealIfLeapYear(1900)).toBe(false);
+        expect(unit.revealIfLeapYear(1700)).toBe(false);
+    });
+
+    it("should return true for an atypical leap year", () => {
+        expect(unit.revealIfLeapYear(2000)).toBe(true);
+        expect(unit.revealIfLeapYear(2400)).toBe(true);
     });
 
 })
