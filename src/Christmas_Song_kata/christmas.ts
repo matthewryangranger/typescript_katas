6192d8570ed39b8f63a7ce1921526a5e2f521818
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-09 13:31:38 -0700

    @ r Refactored first day stanza

--- src/Christmas_Song_kata/christmas.ts
+++ src/Christmas_Song_kata/christmas.ts
@@ -1,5 +1,15 @@
-export function christmas(): void {
+export function christmas(p0: number = 0): void {
+    if (p0 >= 1) {
+        firstDay();
+        beginningLines();
+    }
+}
+
+export function firstDay(){
+    console.log("A partridge in a pear tree.");
+}
+
+export function beginningLines(){
     console.log("On the first day of Christmas");
     console.log("My true love sent to me:");
-    console.log("A partridge in a pear tree.");
 }
--- tests/Christmas_Song_kata_tests/christmas.test.ts
+++ tests/Christmas_Song_kata_tests/christmas.test.ts
@@ -3,7 +3,7 @@ import {christmas} from "../../src/Christmas_Song_kata/christmas";
 describe('christmas song function', () => {
     it('should print to the console the fiirst day stanza', () => {
         const logSpy = jest.spyOn(console, 'log');
-        christmas();
+        christmas(1);
         expect(logSpy).toHaveBeenCalled();
         expect(logSpy).toHaveBeenCalledTimes(3);
         expect(logSpy).toHaveBeenCalledWith("On the first day of Christmas");
