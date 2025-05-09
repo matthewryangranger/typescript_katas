5aee9dacf7ef2039554f3f915220e3023fd4763b
Matthew Ryan Granger <mattg1827@gmail.com>
2025-05-09 13:59:35 -0700

    @ f Enhanced design with switch statement and string day passing for stanza lines

--- src/Christmas_Song_kata/christmas.ts
+++ src/Christmas_Song_kata/christmas.ts
@@ -1,15 +1,25 @@
-export function christmas(p0: number = 0): void {
-    if (p0 >= 1) {
-        firstDay();
-        beginningLines();
-    }
+function secondDay() {
+    console.log("Two turtle doves and");
+    firstDay();
 }
 
-export function firstDay(){
+export function firstDay() {
     console.log("A partridge in a pear tree.");
 }
 
-export function beginningLines(){
-    console.log("On the first day of Christmas");
+export function beginningLines(day: string) {
+    console.log(`On the ${day} day of Christmas`);
     console.log("My true love sent to me:");
+}
+
+export function christmas(p0: number = 0): void {
+    switch (p0) {
+        case 1:
+            beginningLines("first");
+            firstDay();
+            break;
+        case 2:
+            beginningLines("second");
+            secondDay();
+    }
 }
--- tests/Christmas_Song_kata_tests/christmas.test.ts
+++ tests/Christmas_Song_kata_tests/christmas.test.ts
@@ -12,14 +12,15 @@ describe('christmas song function', () => {
         logSpy.mockRestore();
     });
 
-    // it('should print to the console the fiirst day stanza', () => {
-    //     const logSpy = jest.spyOn(console, 'log');
-    //     christmas();
-    //     expect(logSpy).toHaveBeenCalled();
-    //     expect(logSpy).toHaveBeenCalledTimes(3);
-    //     expect(logSpy).toHaveBeenCalledWith("On the first day of Christmas");
-    //     expect(logSpy.mock.calls).toContainEqual(["My true love sent to me:"]);
-    //     expect(logSpy.mock.calls).toContainEqual(["A partridge in a pear tree."]);
-    //     logSpy.mockRestore();
-    // });
+    it('should print to the console the second day stanza', () => {
+        const logSpy = jest.spyOn(console, 'log');
+        christmas(2);
+        expect(logSpy).toHaveBeenCalled();
+        expect(logSpy).toHaveBeenCalledTimes(4);
+        expect(logSpy).toHaveBeenCalledWith("On the second day of Christmas");
+        expect(logSpy.mock.calls).toContainEqual(["My true love sent to me:"]);
+        expect(logSpy.mock.calls).toContainEqual(["Two turtle doves and"]);
+        expect(logSpy.mock.calls).toContainEqual(["A partridge in a pear tree."]);
+        logSpy.mockRestore();
+    });
 });
